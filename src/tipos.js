66f70638cb16634563b9c8f35a89fe3cb4b53ad4
OneWinterSigh <cabelloruizgerman@gmail.com>
2023-11-18 12:45:09 +0100

    Añadido bin y quitados los .js

--- src/tipos.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Tipos;
-(function (Tipos) {
-    Tipos["Asignatura"] = "Asignatura";
-    Tipos["Entrega"] = "Entrega";
-    Tipos["Examen"] = "Examen";
-})(Tipos || (Tipos = {}));
-exports.default = Tipos;
